869d7319ca749fc66f6ec9f0c53bda3b761d9b29
Valerian Ha <ha.valerian@gmail.com>
2024-07-09 09:41:20 -0400

    Finish leapYears js exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYear = false;
+    if (year % 4 == 0) {
+        leapYear = true;
+        if (year % 100 == 0 && year % 400 != 0) {
+            leapYear = false;
+        }
+    }
+    return leapYear;
 };
 
 // Do not edit below this line
