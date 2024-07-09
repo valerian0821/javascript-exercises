f3eb7140c3ff97c6658125501d795a3608a85879
Valerian Ha <ha.valerian@gmail.com>
2024-07-09 11:11:54 -0400

    Finish tempConversion js exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5 / 9);
+  cTemp = Number(cTemp.toFixed(1));
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = cTemp * (9 / 5) + 32;
+  fTemp = Number(fTemp.toFixed(1));
+  return fTemp;
 };
 
 // Do not edit below this line
