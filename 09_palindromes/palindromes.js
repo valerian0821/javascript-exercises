cb9aec9354153a7ee881b7e052b70b0fcb964266
Valerian Ha <ha.valerian@gmail.com>
2024-07-15 17:03:30 -0400

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.trim().toLowerCase();
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i);
+        if ((charCode > 47 && charCode < 58) || (charCode > 96 && charCode < 123) || (charCode > 64 && charCode < 91)) {
+            newStr = newStr + str.charAt(i);    
+        }
+    }
+    let strArray = newStr.split("");
+    let reverseStr = '';
+    for (let i = 0; i < newStr.length; i++) {
+        reverseStr = reverseStr + strArray.pop();
+    }
+    return reverseStr === newStr;
 };
 
 // Do not edit below this line
