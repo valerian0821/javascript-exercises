94acaf9a2a2c490f08e3b1ff6f4e9fd8277db939
Valerian Ha <ha.valerian@gmail.com>
2024-07-16 09:35:16 -0400

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    let arr = [];
+    sequence = Number(sequence);
+    if (sequence === 0) {
+        return 0;
+    } else if (sequence < 0) {
+        return "OOPS";
+    }
+    for (let i = 0; i < sequence; i++) {
+        if (arr.length < 2) {
+            arr.push(1);
+        } else {
+            arr.push(arr[i - 1] + arr[i - 2]);
+        }
+    }
+    return arr[sequence - 1];
 };
 
 // Do not edit below this line
